cda45f4d400bf5bade1541d25f5b4692aeccf101
Angel Morales Chocue <angelmrles7@gmail.com>
2023-11-01 10:23:06 -0500

    feat: protect routes in Navbar and add logout button

--- src/components/layouts/Navbar/components/UserActions/UserActions.tsx
+++ src/components/layouts/Navbar/components/UserActions/UserActions.tsx
@@ -1,29 +1,45 @@
 import { LuLogIn } from 'react-icons/lu';
 import { Link } from 'react-router-dom';
+import { useAuthActions } from '../../../../../hooks';
+import { ROLES } from '../../../../../data/constants';
 
 const UserActions = () => {
+	const { getUserRole, authState, userLogout } = useAuthActions();
+
 	return (
 		<div className='user-actions__popover'>
 			<ul className='user-actions__popover__options'>
-				<li className='user-actions__popover__options__item'>
-					<Link to='/signin'>Sign In</Link>
-				</li>
-				<li className='user-actions__popover__options__item'>
-					<Link to='/signup'>Sign Up</Link>
-				</li>
-				<li className='user-actions__popover__options__item'>
-					<Link to='/dashboard'>Dashboard</Link>
-				</li>
+				{!authState.isLogin && (
+					<>
+						<li className='user-actions__popover__options__item'>
+							<Link to='/signin'>Iniciar sesión</Link>
+						</li>
+						<li className='user-actions__popover__options__item'>
+							<Link to='/signup'>Crear cuenta</Link>
+						</li>
+					</>
+				)}
 
-				<li className='user-actions__popover__options__item'>
-					<button
-						title='Logout'
-						className='user-actions__popover__options__item--logout-btn'
-					>
-						<LuLogIn />
-						Logout
-					</button>
-				</li>
+				{(getUserRole() === ROLES.ADMIN ||
+					getUserRole() === ROLES.MODERATOR) && (
+					<li className='user-actions__popover__options__item'>
+						<Link to='/dashboard'>Dashboard</Link>
+					</li>
+				)}
+
+				{authState.isLogin && (
+					<li className='user-actions__popover__options__item'>
+						<button
+							type='button'
+							title='logout'
+							className='logout-btn'
+							onClick={() => userLogout()}
+						>
+							<LuLogIn />
+							Cerrar sesión
+						</button>
+					</li>
+				)}
 			</ul>
 		</div>
 	);
--- src/components/layouts/Navbar/components/UserActions/UserActionsMobile.tsx
+++ src/components/layouts/Navbar/components/UserActions/UserActionsMobile.tsx
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import AsideNav from '../AsideNav/AsideNav';
 import { AsideNavContent } from '../AsideNav/components/AsideNavContent';
 import { AsideNavItem } from '../AsideNav/components/components/AsideNavItem';
+import { ROLES } from '../../../../../data/constants';
+import { useAuthActions } from '../../../../../hooks';
+import { LuLogIn } from 'react-icons/lu';
 
 interface UserActionsMobileProps {
 	openPanel: boolean;
@@ -12,6 +15,7 @@ export const UserActionsMobile: React.FC<UserActionsMobileProps> = ({
 	openPanel,
 	closePanel,
 }) => {
+	const { getUserRole, authState, userLogout } = useAuthActions();
 	return (
 		<AsideNav
 			title='Cuenta'
@@ -20,15 +24,35 @@ export const UserActionsMobile: React.FC<UserActionsMobileProps> = ({
 			closePanel={closePanel}
 		>
 			<AsideNavContent>
-				<AsideNavItem>
-					<Link to='/signin'>Sign In</Link>
-				</AsideNavItem>
-				<AsideNavItem>
-					<Link to='/signup'>Sign Up</Link>
-				</AsideNavItem>
-				<AsideNavItem>
-					<Link to='/dashboard'>DashBoard</Link>
-				</AsideNavItem>
+				{!authState.isLogin && (
+					<>
+						<AsideNavItem>
+							<Link to='/signin'>Sign In</Link>
+						</AsideNavItem>
+						<AsideNavItem>
+							<Link to='/signup'>Sign Up</Link>
+						</AsideNavItem>
+					</>
+				)}
+				{(getUserRole() === ROLES.ADMIN ||
+					getUserRole() === ROLES.MODERATOR) && (
+					<AsideNavItem>
+						<Link to='/dashboard'>DashBoard</Link>
+					</AsideNavItem>
+				)}
+				{authState.isLogin && (
+					<AsideNavItem>
+						<button
+							type='button'
+							title='logout'
+							className='logout-btn'
+							onClick={() => userLogout()}
+						>
+							<LuLogIn />
+							Cerrar sesión
+						</button>
+					</AsideNavItem>
+				)}
 			</AsideNavContent>
 		</AsideNav>
 	);
